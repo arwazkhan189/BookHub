71ff5c955b2a77f0b89c1f1e5ac7a1e387d548ec
Izhan Ali <mohdizhanali@gmail.com>
2021-09-06 22:01:55 +0530

    added theme switcher

--- app/src/main/java/me/arwazkhan/bookhub/activity/MainActivity.kt
+++ app/src/main/java/me/arwazkhan/bookhub/activity/MainActivity.kt
@@ -1,14 +1,18 @@
 package me.arwazkhan.bookhub.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.FrameLayout
-import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.navigation.NavigationView
 import me.arwazkhan.bookhub.*
 import me.arwazkhan.bookhub.fragment.AboutAppFragment
@@ -101,6 +105,29 @@ class MainActivity : AppCompatActivity() {
         val id = item.itemId
         if (id == android.R.id.home) {
             drawerLayout.openDrawer(GravityCompat.START)
+        } else if (id == R.id.theme_switcher) {
+            val materialAlertDialogBuilder = MaterialAlertDialogBuilder(this)
+            materialAlertDialogBuilder.setTitle("Choose Mode")
+            materialAlertDialogBuilder.setIcon(R.drawable.ic_theme)
+            val modes = arrayOf("Auto (System Default)", "Dark mode", "Light mode")
+            materialAlertDialogBuilder.setSingleChoiceItems(modes, this.getPreferences(Context.MODE_PRIVATE).getInt(getString(R.string.themekey),0)) { dialog, i -> }
+            materialAlertDialogBuilder.setPositiveButton("OK") { dialog, which ->
+                // if selected, then get item text
+                val pos = (dialog as AlertDialog).listView.checkedItemPosition
+                val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return@setPositiveButton
+                with (sharedPref.edit()) {
+                    putInt(getString(R.string.themekey),pos )
+                    apply()
+                }
+                when(pos){
+                    0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    2 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+                }
+
+            }
+            materialAlertDialogBuilder.show()
         }
         return super.onOptionsItemSelected(item)
     }
