1d5fd754a7bb4b04cdb6241eb06c16197259799c
PranavVyas20 <Pranavvyas2003@gmail.com>
2021-09-19 16:38:21 +0530

    first commit

--- app/src/main/java/me/arwazkhan/bookhub/activity/SplashScreen.kt
+++ app/src/main/java/me/arwazkhan/bookhub/activity/SplashScreen.kt
@@ -19,7 +19,7 @@ class SplashScreen : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        },1000)
 
     }
 }
